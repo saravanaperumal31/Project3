c792cf77da246513fbcac65dccd854f0671541e2
saravanaperumal31 <saravanaperumalofficial@gmail.com>
2021-02-27 22:36:13 +0530

    new project

---
+++ src/org/tcs/login/Computer.java
@@ -0,0 +1,9 @@
+package org.tcs.login;
+
+import java.awt.Desktop;
+
+public class Computer {
+public void computerModel() {
+	System.out.println("the computer model is DELL");
+}
+}
---
+++ src/org/tcs/login/Desktop.java
@@ -0,0 +1,19 @@
+package org.tcs.login;
+
+public class Desktop  extends Computer{
+private void desktopSize() {
+	System.out.println("The desktop size is 14inch");
+	super.computerModel();
+	
+	
+}
+public void computerModel() {
+	System.out.println("hjvhjvgriuvr");
+}
+
+public static void main(String[] args) {
+	Desktop q=new Desktop();
+	q.computerModel();
+	q.desktopSize();
+	
+}}
