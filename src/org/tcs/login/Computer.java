abddb1ca7250c479ddcfd8c7afef664ddac50489
saravanaperumal31 <saravanaperumalofficial@gmail.com>
2021-02-27 23:28:49 +0530

    megred

--- src/org/tcs/login/Computer.java
+++ src/org/tcs/login/Computer.java
@@ -5,13 +5,11 @@ import java.awt.Desktop;
 public class Computer {
 public void computerModel() {
 	System.out.println("the computer model is DELL");
-<<<<<<< HEAD
-	System.out.println("The color is Black silver");
-=======
+	
 	System.out.println("The computer price is 37000");
 	System.out.println("The weight is 1.5kg");
 	System.out.println("hhauaia");
 	System.out.println("KADGDGDgF");
->>>>>>> 5ee24393ddc854fa6909820b60255bd89062c25d
+System.out.println("The color is Black silver");
 }
 }
